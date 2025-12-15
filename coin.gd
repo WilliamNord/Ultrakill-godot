82ed195ed4f8ac06f603d394fefb29f2dc728472
WilliamNord <williamnordberg08@gmail.com>
2025-12-15 09:15:56 +0100

    Initial Godot project setup with player and coin
    Add initial Godot project files including player and coin scenes, scripts, and assets. Includes basic player movement and coin spawning functionality.

---
+++ coin.gd
@@ -0,0 +1,9 @@
+extends RigidBody2D
+
+
+func _ready():
+	pass
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ player.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+
+const SPEED = 350.0
+const JUMP_VELOCITY = -500.0
+const COIN_INSTANCE = preload("res://coin.tscn")
+@onready var world: Node2D = $".."
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if Input.is_action_just_pressed("click"):
+		var COIN = COIN_INSTANCE.instantiate()
+		world.add_child(COIN)
+		#spawner coin litt over spilleren
+		COIN.global_position = self.global_position + Vector2(0,-100)
+		COIN.sleeping = false
+		COIN.apply_impulse(Vector2.ZERO, Vector2.UP * 200)
+		print("coin toss")
+	move_and_slide()
